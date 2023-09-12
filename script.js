b0cc2de8b1620f7e480d0372c0740686928d15eb
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-12 21:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  // Sort the input array
+  arr.sort((a, b) => a - b);
+
+  // Initialize variables
+  let closestSum = Infinity;
+  let minDiff = Infinity;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    // Use two-pointer approach for the remaining elements
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      const diff = Math.abs(sum - target);
+
+      if (diff < minDiff) {
+        // Update closest sum and minDiff
+        closestSum = sum;
+        minDiff = diff;
+      }
+
+      if (sum < target) {
+        // Move the left pointer to the right
+        left++;
+      } else {
+        // Move the right pointer to the left
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
 }
+
 
 module.exports = threeSum;
